533f095fbe64af0c3e83735ec34ecfc72a0fe2c4
Jerry Zhang <jerryfish@163.com>
2022-06-28 16:41:48 +1200

    Create store root type
    feat: Create store root type and set up a value in store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { RootState } from './types'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+export default new Vuex.Store<RootState>({
   state: {
+    welcomeMessage: 'This is homepage of Connecxionz task.',
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
 })
