2ebc24463f70bafc6da17e68de6cc81730fc1baa
jerryfishzz <jerryfish@163.com>
2022-06-28 21:45:07 +1200

    Use store to provide the content
    feat: Use store to provide the content for homepage
    
    - Use store to provide the content for homepage
    
    - Create the template on homepage directly

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,14 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, { StoreOptions } from 'vuex'
 import { RootState } from './types'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store<RootState>({
+const store: StoreOptions<RootState> = {
   state: {
-    welcomeMessage: 'This is homepage of Connecxionz task.',
+    welcomeMessage: 'This is homepage of Connecxionz task',
   },
-  getters: {},
-  mutations: {},
-  actions: {},
   modules: {},
-})
+}
+
+export default new Vuex.Store<RootState>(store)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,25 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row class="text-center">
+      <v-col>
+        <h1>Home</h1>
+        <p>{{ welcomeMessage }}</p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import HelloWorld from '../components/HelloWorld.vue'
+import Vue from 'vue'
 
-  export default Vue.extend({
-    name: 'HomeView',
-
-    components: {
-      HelloWorld,
+export default Vue.extend({
+  name: 'HomeView',
+  computed: {
+    welcomeMessage: {
+      get() {
+        return this.$store.state.welcomeMessage
+      },
     },
-  })
+  },
+})
 </script>
