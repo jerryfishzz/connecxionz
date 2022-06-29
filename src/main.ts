b749f5505cdf1c1135b8a4152e3afe58b4d292e7
Jerry Zhang <jerryfish@163.com>
2022-06-29 12:29:01 +1200

    Organize main.ts file
    chore: Organize main.ts file

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,9 @@ Vue.config.productionTip = false
 
 Vue.use(VueLayers)
 
-// Register a global custom directive called `v-blur` that prevents focus
-// For example, to use this directive on buttons opening dialog // since they should not keep focusing after the dialog disappears
+// Register a global custom directive called `v-blur` that prevents focus.
+// For example, to use this directive on buttons opening dialog
+// since they should not keep focusing after the dialog disappears.
 // https://github.com/vuetifyjs/vuetify/issues/8572
 Vue.directive('blur', {
   inserted: function (el: HTMLElement) {
